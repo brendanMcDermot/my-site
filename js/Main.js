f00b9db6687e7c753daf671d3c6e5cb8df50b31f
brendanMcDermot <62312017+brendanMcDermot@users.noreply.github.com>
2021-02-15 23:40:16 +1300

    big canvas update

--- js/Main.js
+++ js/Main.js
@@ -1,21 +1,21 @@
 var canvas = document.querySelector('canvas');
-
-var c = canvas.getContext('2d');
-var view = new View(c);
 var mouse = {
     x: undefined,
-    y: undefined
+    y: undefined,
+    radius: 50
 }
-var maxRadius = 40;
-var numberOfActors = 800;
-var actorSpeed = 2;
-var controller = new Controller(view, innerWidth, innerHeight, mouse);
+var time = 0;
+var c = canvas.getContext('2d');
+var view = new View(c);
+var controller = new Controller(view, window.innerWidth, window.innerHeight, mouse, canvas);
 
 //size the canvas
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight / 1.6;
+canvas.height = window.innerHeight;
+
 
 // event listeners
+// find the cursor
 window.addEventListener('mousemove', 
     function(event){
         mouse.x = event.x;
@@ -25,19 +25,39 @@ window.addEventListener('mousemove',
 // resizing the window (responsive)
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight / 1.6;
-
-    controller.init(numberOfActors, innerWidth, innerHeight, actorSpeed);
+    canvas.height = window.innerHeight;
+    controller.init(innerWidth, innerHeight);
 })
+
+canvas.addEventListener('mousedown', e => {
+    if(this.controller.state.transition == false){
+        this.controller.state.transition = true
+    }
+  });
+  
+
+// counts the seconds
+setInterval(function(){
+time += 1
+if(time > 350){
+    time = 0
+}
+// console.log(time)
+}, 100)
 
 // animation loop
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    controller.animateLoop();
+    controller.animateLoop(time);
 }
 
-controller.init(numberOfActors, innerWidth, innerHeight, actorSpeed);
+
+
+controller.init(innerWidth, innerHeight);
+console.log(controller)
+console.log('inner height = ' + innerHeight)
+console.log('inner width = ' + innerWidth)
 animate();
 
 
--- js/View.js
+++ js/View.js
@@ -6,9 +6,9 @@ class View{
     draw(actor){
         c.beginPath();
         c.arc(actor.x, actor.y, actor.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = "blue";
+        c.strokeStyle = actor.outlineToString();
         c.stroke();
-        c.fillStyle = actor.color;
+        c.fillStyle = actor.colorToString();
         c.fill();
     }
 }
