31ac1467f503746a91e72f971a0a093707409c89
Brendan <bjm0412@arastudent.ac.nz>
2021-08-28 03:23:56 +0100

    about changes

--- js/About.js
+++ js/About.js
@@ -1,10 +1,11 @@
 const fader = document.getElementById('fader')
+const profilePic = document.getElementById('profile_pic')
+let profFromLeft = -20
 let isLoaded = false
 let aboutState = new AboutState()
 
 function animate() {
     requestAnimationFrame(animate);
-    console.log('animate running')
     if(isLoaded == false){
         fader.style.background = 'rgba(200, 200, 200, 1)'
         isLoaded = true
@@ -15,6 +16,11 @@ function animate() {
         }else{
             fader.style.zIndex  = "-2"
         }
+    }
+    if(profFromLeft < 50){
+        console.log(profilePic.style.x)
+        profilePic.style.left = `${profFromLeft}px`
+        profFromLeft = decelerate(profFromLeft)
     }
 }
 
--- js/algorithmic.js
+++ js/algorithmic.js
@@ -2,3 +2,8 @@ function insertNameLater(x, y){
     y = x
     return y
 }
+
+function decelerate(x){
+    x += 1
+    return Math.floor(x)
+}
