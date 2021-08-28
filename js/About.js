138cad7120326400fbd7692bb6c92c5b914c5b07
Brendan <bjm0412@arastudent.ac.nz>
2021-08-28 03:35:56 +0100

    about errors

--- js/About.js
+++ js/About.js
@@ -2,26 +2,30 @@ const fader = document.getElementById('fader')
 const profilePic = document.getElementById('profile_pic')
 let profFromLeft = -20
 let isLoaded = false
+let isAnimate = true
 let aboutState = new AboutState()
 
 function animate() {
-    requestAnimationFrame(animate);
-    if(isLoaded == false){
-        fader.style.background = 'rgba(200, 200, 200, 1)'
-        isLoaded = true
-    }else{
-        fader.style.background = `rgba(200, 200, 200, ${aboutState.faderValue})`
-        if(aboutState.faderValue > 0){
-            aboutState.faderValue -= 0.01
+    if(isAnimate){
+        requestAnimationFrame(animate);
+        if(isLoaded == false){
+            fader.style.background = 'rgba(200, 200, 200, 1)'
+            isLoaded = true
         }else{
-            fader.style.zIndex  = "-2"
+            fader.style.background = `rgba(200, 200, 200, ${aboutState.faderValue})`
+            if(aboutState.faderValue > 0){
+                aboutState.faderValue -= 0.01
+            }else{
+                fader.style.zIndex  = "-2"
+            }
         }
+        if(profFromLeft < 50){
+            console.log(profilePic.style.x)
+            profilePic.style.left = `${profFromLeft}px`
+            profFromLeft = decelerate(profFromLeft)
+        }        
     }
-    if(profFromLeft < 50){
-        console.log(profilePic.style.x)
-        profilePic.style.left = `${profFromLeft}px`
-        profFromLeft = decelerate(profFromLeft)
-    }
+
 }
 
 animate()
